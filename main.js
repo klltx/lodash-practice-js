97c6f88b024eab1b6200f242f12adbb7baf04ecf
klltx <kostya.svetashov.01@gmail.com>
2021-10-05 18:48:42 +0400

    comment: add chunk description

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 
+/* 
+Creates an array of elements split into groups the length of size.
+  If array can't be split evenly, the final chunk will be the remaining elements.
+*/
 const _chunk = (array, size = 1) => {
   return array
 }
