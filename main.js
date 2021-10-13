67fe12bc4b749d4bcd7a7829e8f76934e065c57d
klltx <kostya.svetashov.01@gmail.com>
2021-10-13 13:12:00 +0400

    feat: add compact function

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-
+"use strict";
 /* 
 Creates an array of elements split into groups the length of size.
 If array can't be split evenly, the final chunk will be the remaining elements.
 */
 const _chunk = (array, size = 1) => {
   const chunkedArray = [],
-    chunkSize = size,
     chunkedArraySize = Math.ceil(array.length / size);
     
   let elemIndex = 0;
 
   for(let i = 0; i < chunkedArraySize; i++){
     chunkedArray[i] = [];
-    for(let j = 0; j < chunkSize; j++){
+    for(let j = 0; j < size; j++){
       chunkedArray[i][j] = array[elemIndex];
       elemIndex++;
       if(elemIndex >= array.length) return chunkedArray;
@@ -27,12 +26,17 @@ Creates an array with all falsey values removed.
 The values false, null, 0, "", undefined, and NaN are falsey.
 */
 const _compact = (array) => {
-  return array
+  const compactArray = []
+  array.forEach((value) => { if(value) compactArray.push(value) })
+  return compactArray
 }
 
 
 
 const main = (args) => {
-  console.log(_chunk(['a', 'b', 'c', 'd'], 2));
+  // console.log(_chunk(['a', 'b', 'c', 'd'], 2));
+  console.log(_compact([0, 1, false, 2, '', 3, undefined, NaN]));
   return args;
 }
+
+main()
