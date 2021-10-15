bec98d34a6dbb119ef928031edba67bdbd180299
klltx <kostya.svetashov.01@gmail.com>
2021-10-15 13:00:44 +0400

    feat: add difference function

--- main.js
+++ main.js
@@ -7,46 +7,66 @@ If array can't be split evenly, the final chunk will be the remaining elements.
 const _chunk = (array, size = 1) => {
   const chunkedArray = [],
     chunkedArraySize = Math.ceil(array.length / size);
-    
+
   let elemIndex = 0;
 
-  for(let i = 0; i < chunkedArraySize; i++){
+  for (let i = 0; i < chunkedArraySize; i++) {
     chunkedArray[i] = [];
-    for(let j = 0; j < size; j++){
+    for (let j = 0; j < size; j++) {
       chunkedArray[i][j] = array[elemIndex];
       elemIndex++;
-      if(elemIndex >= array.length) return chunkedArray;
+      if (elemIndex >= array.length) return chunkedArray;
     }
   }
 
   return chunkedArray;
-}
+};
 
 /* 
 Creates an array with all falsey values removed. 
 The values false, null, 0, "", undefined, and NaN are falsey.
 */
 const _compact = (array) => {
-  const compactArray = []
-  array.forEach((value) => { if(value) compactArray.push(value) })
-  return compactArray
-}
+  const compactArray = [];
+  array.forEach((value) => {
+    if (value) compactArray.push(value);
+  });
+  return compactArray;
+};
 
 /*
 Creates a new array concatenating array with any additional arrays and/or values.
 */
 const _concat = (array, ...values) => {
-  return [ ...array, ...values ]
-}
+  return [...array, ...values];
+};
 
+/*
+Creates an array of array values not included in the other given arrays using
+SameValueZero for equality comparisons.
+The order and references of result values are determined by the first array.
+*/
+const _difference = (array, values) => {
+  const resultArray = [];
+
+  array.forEach((v) => {
+    if (!values.includes(v)) resultArray.push(v);
+  });
+
+  return resultArray;
+};
 
 const main = (args) => {
   // console.log(_chunk(['a', 'b', 'c', 'd'], 2));
+
   // console.log(_compact([0, 1, false, 2, '', 3, undefined, NaN]));
 
-  const array = [1];
-  console.log(_concat(array, 2, [3], [[4]]));
-  return args;
-}
+  // const array = [1];
+  // console.log(_concat(array, 2, [3], [[4]]));
 
-main()
+  console.log(_difference([2, 1], [2, 3]));
+
+  return args;
+};
+
+main();
